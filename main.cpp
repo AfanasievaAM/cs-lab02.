b572464a758c86d1e1922d3f003117919a13dc34
Bob (AfanasievaAM) <arinafanasieva@mail.ru>
2020-03-18 17:37:07 +0300

    code: вывод минимума

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@ using namespace std;
 int main()
 {
   cout << "Enter A and B: ";
-  int a, b, max;
+  int a, b, max,min;
   cin >> a >> b;
   cout << "A + B = " << a + b << '\n';
   cout << "A - B = " << a - b << '\n';
   cout << "A * B = " << a * b << '\n';
   cout << "A / B = " << a / b << '\n';
   max=0;
+  min=0;
   if (a>b)
-      max=a;
-      else
-      max=b;
-  cout << max;
+max=a;
+if (b<a)
+    min=b;
+     cout << min<<'\n';
+  cout<< max;
     return 0;
 }
+
